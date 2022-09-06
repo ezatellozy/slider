4b10e23e8c8e3a71c8c64bf37f9857abbaaff866
Ezat MAc <ezatellozy1@gmail.com>
2022-09-07 00:53:07 +0200

    all

--- js/main.js
+++ js/main.js
@@ -92,8 +92,7 @@
 
                 // Mobile Navigation
                 self.mobileNav();
-
-
+                self.projectFilter();
 
 
             } );
@@ -146,6 +145,52 @@
             $(document).on('click', '#main-nav-mobi .arrow', function () {
                 $(this).toggleClass('active').next().slideToggle();
             })
+        },
+        projectFilter: function() {
+            $('.wprt-project').each(function() {
+                var
+                $this = $(this),
+                item = $this.data("column"),
+                item2 = $this.data("column2"),
+                item3 = $this.data("column3"),
+                item4 = $this.data("column4"),
+                layout = $this.data("layout"),
+                gapH = Number($this.data("gaph")),
+                    gapV = Number($this.data("gapv"));
+
+                    $(this).find('#projects').cubeportfolio({
+                    filters: '#project-filter',
+                    layoutMode: slider,
+                    defaultFilter: '*',
+                    animationType: 'quicksand',
+                    gapHorizontal: gapH,
+                    gapVertical: gapV,
+                    showNavigation: true,
+                    showPagination: true,
+                    gridAdjustment: 'responsive',
+                    rewindNav: false,
+                    auto: false,
+                    mediaQueries: [{
+                        width: 1500,
+                        cols: item
+                    }, {
+                        width: 1100,
+                        cols: item
+                    }, {
+                        width: 800,
+                        cols: item2
+                    }, {
+                        width: 550,
+                        cols: item3
+                    }, {
+                        width: 320,
+                        cols: item4
+                    }],
+                    caption: ' ',
+                    displayType: 'bottomToTop',
+                    displayTypeSpeed: 100
+                });
+            });
         },
     }
     wprtTheme.init();
